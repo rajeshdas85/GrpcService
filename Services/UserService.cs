6cb805c5c4af989d2835fc850e5affdac663e5da
rajesh das <rajesh.das@evolutyz.com>
2021-03-12 10:10:09 +0530

    Add project files.

---
+++ Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using GrpcServiceDemo;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServiceDemo
+{
+    public class UserService:Users.UsersBase
+    {
+        private readonly ILogger<UserService> _logger;
+        public UserService(ILogger<UserService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<UsersReply> GetUsers(UsersRequest request, ServerCallContext context)
+        {
+            IEnumerable<User> users = Enumerable.Range(1, 10000).Select(
+                x => new User { Id = x, Name = $"Name {x} from {request.DepartmentName}" });
+            UsersReply response = new UsersReply();
+            response.ListOfUsers.AddRange(users);
+            return Task.FromResult(response);
+        }
+    }
+}
